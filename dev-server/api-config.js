534261b5ee92f0ad24c79afb1257045ff3d0d150
marcusrc <marcus.chen@ringcentral.com>
2017-05-07 22:44:19 +0800

    add config

---
+++ dev-server/api-config.js
@@ -0,0 +1,5 @@
+export default {
+  appKey: 'eac8797af1b3502F2CEAAEECAC3Ed378AA7858A386656f28A008b0c638A754B1',
+  appSecret: 'c082702E4ea4DA18c4b1377917778a8aafabCA3Be579B78B66d17C36874b27F4',
+  server: 'https://api-up.lab.rcch.ringcentral.com',
+};
